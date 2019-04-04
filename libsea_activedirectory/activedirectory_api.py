1533a7c81c4f8f31a45dcde6d71feb90608bc511
Jerod Gawne <jerodgawne@gmail.com>
2019-04-04 15:00:45 -0400

    Update activedirectory_api.py
    Add get_computers();
    fix get_users()

--- libsea_activedirectory/activedirectory_api.py
+++ libsea_activedirectory/activedirectory_api.py
@@ -5,74 +5,94 @@ from sys import argv, exc_info
 from traceback import print_exception
 from typing import List, Optional
 
-from jgutils.replace import replace as rpl
-from ldap3 import ALL, Connection, NTLM, Server
+from ldap3 import ALL, ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES, Connection, NTLM, Server
 from os import getenv
 from os.path import abspath, basename, dirname
-from tenacity import after_log, retry, retry_if_exception_type, wait_random_exponential
-from tqdm import tqdm
+from tenacity import after_log, before_sleep_log, retry, retry_if_exception_type, stop_after_attempt, wait_random_exponential
 
-from libsea_activedirectory.activedirectory_data import ADUser
 from libsea_base.base_api import ApiBase
 
 logger = logging.getLogger(__name__)
 DBG = logger.isEnabledFor(logging.DEBUG)
 NFO = logger.isEnabledFor(logging.INFO)
-RETRY: int = 5
 
-
-# todo: remove tqdn
 
 class ActiveDirectoryApi(ApiBase):
     ROOT: str = dirname(abspath(__file__))
     SEM: int = 100
-    AD_ATTR: List[str] = ['badPwdCount', 'cn', 'department', 'displayName', 'manager', 'memberOf', 'title']
+    USER_ATTR: List[str] = ['badPwdCount', 'cn', 'department', 'displayName', 'manager', 'memberOf', 'title']
+    COMPUTER_ATTR: List[str] = ['Name', 'lastLogonTimestamp']
     AD_REPL: List[str] = ['CN=', 'DN=', 'OU=', 'DC=']
+    DOMAINS: List[str] = ['dm0001', 'dm0007']
 
     def __init__(self, root: str, sem: Optional[int] = None):
         ApiBase.__init__(self, root=root or self.ROOT, sem=sem or self.SEM, parent=basename(argv[0][:-3]))
+        self.adserver: Server = Server(getenv('AD_HOST'), use_ssl=True, get_info=ALL)
 
-    def __enter__(self):
+    def __aenter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __aexit__(self, exc_type, exc_val, exc_tb):
+        self.cache.sync()
         ApiBase.__exit__(self, exc_type, exc_val, exc_tb)
 
     @retry(retry=retry_if_exception_type(ConnectionResetError),
-           wait=wait_random_exponential(max=RETRY),
-           after=after_log(logger, logging.WARNING))
-    async def fetch_ad(self) -> dict:
-        adserver: Server = Server(getenv('AD_HOST'), use_ssl=True, get_info=ALL)
-        adconn: Connection = Connection(adserver, auto_bind=True, user=getenv('AD_USER'),
-                                        password=getenv('AD_PASS'),
-                                        authentication=NTLM)
+           wait=wait_random_exponential(multiplier=1.25, min=3, max=60),
+           after=after_log(logger, logging.DEBUG),
+           stop=stop_after_attempt(7),
+           before_sleep=before_sleep_log(logger, logging.DEBUG))
+    async def get_users(self) -> dict:
+        with Connection(self.adserver, auto_bind=True, user=getenv('AD_USER'),
+                        password=getenv('AD_PASS'),
+                        authentication=NTLM) as adconn:
+            entries = adconn.extend.standard.paged_search('dc=dm0001,dc=info53,dc=com', '(objectClass=person)',
+                                                          attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES], paged_size=100)
 
-        entries = adconn.extend.standard.paged_search('dc=dm0001,dc=info53,dc=com', '(objectClass=person)',
-                                                      attributes=self.AD_ATTR, paged_size=25)
-        try:
-            adcnt = self.cache['ad_count']
-        except KeyError:
-            adcnt = 100000  # Base was 97400; this is an estimate
+        # for entry in entries:
+        #     e = entry['attributes']
+        #     ident = e['cn']
+        #     name = e['displayName'] if e['displayName'] else ''
+        #     memberof = set(rpl(''.join(e['memberOf']), self.AD_REPL, '').split(','))
+        #     department = [d.lower() for d in e['department']] if type(e['department']) is list else e['department']
+        #     manager = rpl(''.join(e['manager']), self.AD_REPL, '').split(',')[0].lower()
+        #     title = e['title']
+        #
+        #     users.append(User(ident=ident,
+        #                       name=name,
+        #                       memberof=memberof,
+        #                       department=department,
+        #                       manager=manager,
+        #                       title=title).dict())
 
-        ad_users: list = []
+        return await self.process_results(results=[e['attributes'] for e in entries])
 
-        for entry in tqdm(entries, desc='Processing AD', unit=' records', total=adcnt):
-            e = entry['attributes']
-            ident = e['cn']
-            name = e['displayName'] if e['displayName'] else ''
-            memberof = set(rpl(''.join(e['memberOf']), self.AD_REPL, '').split(','))
-            department = [d.lower() for d in e['department']] if type(e['department']) is list else e['department']
-            manager = rpl(''.join(e['manager']), self.AD_REPL, '').split(',')[0].lower()
-            title = e['title']
+    @retry(retry=retry_if_exception_type(ConnectionResetError),
+           wait=wait_random_exponential(multiplier=1.25, min=3, max=60),
+           after=after_log(logger, logging.DEBUG),
+           stop=stop_after_attempt(7),
+           before_sleep=before_sleep_log(logger, logging.DEBUG))
+    async def get_computers(self, last_logon: int = 7, domains: List[str] = None) -> dict:
+        with Connection(self.adserver, auto_bind=True, user=getenv('AD_USER'),
+                        password=getenv('AD_PASS'),
+                        authentication=NTLM) as adconn:
+            entries = adconn.extend.standard.paged_search('dc=dm0001,dc=info53,dc=com', '(objectClass=computer)',
+                                                          attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES], paged_size=100)
 
-            ad_users.append(ADUser(ident=ident,
-                                   name=name,
-                                   memberof=memberof,
-                                   department=department,
-                                   manager=manager,
-                                   title=title).dict())
+            # for entry in entries:
+            #     try:
+            #         e = entry['attributes']
+            #         last_log = e['lastLogonTimestamp']
+            #     except KeyError:
+            #         print('entry has no attributes:\n\t', entry)
+            #         continue
+            #
+            #     try:
+            #         if last_log >= last_week:
+            #             computers.append(Computer(name=e['name'], last_logon=last_log).dict())
+            #     except TypeError:
+            #         continue
 
-        return await self.process_results(results=ad_users)
+            return await self.process_results(results=[e['attributes'] for e in entries])
 
 
 if __name__ == '__main__':
