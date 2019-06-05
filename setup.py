ae5e682ed2c7bd009505605cfabf0b171d1d4b98
Jerod Gawne <jerodgawne@gmail.com>
2019-06-05 14:48:12 -0400

    Update setup.py
    update format;
    prepare for release

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 """LibSEA ActiveDirectory: Setup
    Jerod Gawne, 2019.03.14 <https://github.com/jerodg/>"""
 import logging
-import setuptools
 import sys
 import traceback
 
+import setuptools
+
 logger = logging.getLogger(__name__)
 name = 'sealib_activedirectory'
+
 
 def readme() -> str:
     with open('README.md') as f:
@@ -17,11 +19,11 @@ def readme() -> str:
 if __name__ == '__main__':
     try:
         setuptools.setup(name='sealib_activedirectory',
-                         version='1.0b0.dev1',
+                         version='1.0.0',
                          description='ActiveDirectory API Client Library',
                          long_description=readme(),
                          long_description_content_type='text/markdown',
-                         classifiers=['Development Status :: 4 - Beta',
+                         classifiers=['Development Status :: 5 - Production/Stable',
                                       'Environment :: Console',
                                       'Intended Audience :: End Users/Desktop',
                                       'Intended Audience :: Developers',
@@ -31,15 +33,24 @@ if __name__ == '__main__':
                                       'Operating System :: MacOS :: MacOS X',
                                       'Operating System :: Microsoft :: Windows',
                                       'Operating System :: POSIX',
-                                      'Programming Language :: Python',
-                                      'Topic :: Utilities'],
-                         keywords='api client base',
-                         url='https://github.info53.com/Fifth-Third/sea_lib_activedirectory',
+                                      'Programming Language :: Python :: 3.7',
+                                      'Topic :: Utilities',
+                                      'Topic :: Internet',
+                                      'Topic :: Internet :: WWW/HTTP'],
+                         keywords='sea lib api client activedirectory',
+                         url='http://slgramihqaims90.info53.com:3030/root/seapypi/sealib-activedirectory/',
                          author='Jerod Gawne',
                          author_email='jerodgawne@gmail.com',
                          license='Other/Proprietary',
                          packages=setuptools.find_packages(),
-                         install_requires=['aiohttp', 'aiodns', 'cchardet', 'libsea_base', 'tenacity'],
+                         install_requires=['aiohttp',
+                                           'aiodns',
+                                           'better-exceptions',
+                                           'cchardet',
+                                           'delorean',
+                                           'ldap3',
+                                           'sealib_base',
+                                           'tenacity'],
                          include_package_data=True,
                          zip_safe=True,
                          setup_requires=['pytest-runner'],
@@ -47,7 +58,9 @@ if __name__ == '__main__':
                          scripts=[],
                          entry_points={'console_scripts': []},
                          python_requires='~=3.7',
-                         project_urls={
-                             'Documentation': 'http://slgramihqaims90.info53.com:3333/docs/sealib_activdirectory'})
+                         project_urls={'Documentation': 'https://github.info53.com/pages/Fifth-Third/sea_lib_activedirectory',
+                                       'Source': 'https://github.info53.com/Fifth-Third/sea_lib_activedirectory',
+                                       'Bugs': 'https://github.info53.com/Fifth-Third/sea_lib_activedirectory/issues'},
+                         package_data={'sealib_activedirectory': ['LICENSE.txt.txt', 'README.md']})
     except Exception as excp:
         logger.exception(traceback.print_exception(*sys.exc_info()))
