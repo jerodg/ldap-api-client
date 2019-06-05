c99744d3244b08714341ad3e4185fc5c251e60f1
Jerod Gawne <jerodgawne@gmail.com>
2019-06-05 14:48:30 -0400

    Update __init__.py
    update format

--- sealib_activedirectory/__init__.py
+++ sealib_activedirectory/__init__.py
@@ -1,17 +1,7 @@
 #!/usr/scripts/env python3.7
-"""LibSEA Base Init: Jerod Gawne, 2019.03.14 <https://github.com/jerodg/>"""
-import logging
-import sys
-import traceback
+"""LibSEA ActiveDirectory: Init
+   Jerod Gawne, 2019.03.14 <https://github.com/jerodg/>"""
+
+from sealib_activedirectory import activedirectory_api, activedirectory_data
 
 ___all___ = ['activedirectory_api', 'activedirectory_data']
-
-logger = logging.getLogger(__name__)
-DBG = logger.isEnabledFor(logging.DEBUG)
-NFO = logger.isEnabledFor(logging.INFO)
-
-if __name__ == '__main__':
-    try:
-        print(__doc__)
-    except Exception as excp:
-        logger.exception(traceback.print_exception(*sys.exc_info()))
